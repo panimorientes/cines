60fe0a8ad9cb9c88210fed2ab559fed7314400b6
absolutyass <absolutyass@f28398b0-52b3-11de-a3da-17e148e7a168>
2009-07-04 14:10:22 +0000

    Arreglada la liberacion de tickets y despues de una busqueda solo se muestran las sesiones actuales y futuras.Tutto apposto.

--- cines/src/main/java/javi/http/controller/actions/LogoutAction.java
+++ cines/src/main/java/javi/http/controller/actions/LogoutAction.java
@@ -8,12 +8,14 @@ import javax.servlet.http.HttpServletResponse;
 
 import javi.http.controller.session.SessionManager;
 import javi.model.adminfacade.delegate.AdminFacadeDelegate;
+import javi.model.userfacade.delegate.UserFacadeDelegate;
 
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 
 import es.udc.fbellas.j2ee.util.exceptions.InternalErrorException;
+import es.udc.fbellas.j2ee.util.exceptions.ModelException;
 import es.udc.fbellas.j2ee.util.struts.action.DefaultAction;
 
 public class LogoutAction extends DefaultAction {
@@ -23,9 +25,22 @@ public class LogoutAction extends DefaultAction {
 			throws IOException, ServletException, InternalErrorException {
 
 		/* Do logout. */
-
+		
+		UserFacadeDelegate userFacadeDelegate = SessionManager.getUserFacadeDelegate(request);
+		
+		//TODO este try-catch tampoco se como va...
+		try {
+			
+			userFacadeDelegate.liberarTickets();
+			
+		} catch (ModelException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		SessionManager.logout(request, response);
 
+	
 		/* Return ActionForward. */
 		return mapping.findForward("MostrarCarteleraForm");
 
--- cines/src/main/java/javi/model/userfacade/plain/PlainUserFacadeDelegate.java
+++ cines/src/main/java/javi/model/userfacade/plain/PlainUserFacadeDelegate.java
@@ -4,7 +4,6 @@ import java.util.Calendar;
 
 import javax.sql.DataSource;
 
-
 import javi.model.carroCompra.CarroCompra;
 import javi.model.carroCompra.CarroNotFoundException;
 import javi.model.direccion.vo.DireccionVO;
@@ -21,6 +20,7 @@ import javi.model.userfacade.plain.actions.FindUserDireccionAction;
 import javi.model.userfacade.plain.actions.FindUserProfileAction;
 import javi.model.userfacade.plain.actions.FindUserTarjetaAction;
 import javi.model.userfacade.plain.actions.LiberarTicketAction;
+import javi.model.userfacade.plain.actions.LiberarTicketsAction;
 import javi.model.userfacade.plain.actions.LoginAction;
 import javi.model.userfacade.plain.actions.RegisterUserAction;
 import javi.model.userfacade.plain.actions.RegistrarTarjetaAction;
@@ -39,260 +39,272 @@ import es.udc.fbellas.j2ee.util.sql.PlainActionProcessor;
 
 public class PlainUserFacadeDelegate implements UserFacadeDelegate {
 
-    private String loginName;
-    private CarroCompra carroCompra;
-  
-    
-    public PlainUserFacadeDelegate() {
-        loginName = null;
-    }
-    
-    public CarroCompra getCarroCompra() throws CarroNotFoundException{
-    	return carroCompra;
-    }
-    
-    public void registerUser(String loginName, String clearPassword, Long cp, String ciudad, String direccion, Long numero,
-        UserProfileDetailsVO userProfileDetailsVO)
-        throws DuplicateInstanceException, InternalErrorException {
-        
-        try {
-        
-            RegisterUserAction action = new RegisterUserAction(loginName, 
-                clearPassword, cp, ciudad, direccion, numero, userProfileDetailsVO);
-                
-            PlainActionProcessor.process(getDataSource(), action);
-            this.carroCompra = new CarroCompra();
-            this.loginName = loginName;
-            
-        } catch (DuplicateInstanceException e) {
-            throw e;
-        } catch (InternalErrorException e) {
-            throw e;
-        } catch (Exception e) {
-            throw new InternalErrorException(e);
-        }
-        
-    }
-        
-    public LoginResultVO login(String loginName, String password,
-        boolean passwordIsEncrypted)
-        throws InstanceNotFoundException, IncorrectPasswordException,
-            InternalErrorException {
+	private String loginName;
+	private CarroCompra carroCompra;
 
-        try {
-        
-            LoginAction action = new LoginAction(loginName, 
-                password, passwordIsEncrypted);
-                
-            LoginResultVO loginResultVO = 
-                (LoginResultVO) PlainActionProcessor.process(getDataSource(),
-                    action);
-            
-            this.loginName = loginName;
-            this.carroCompra = new CarroCompra();
-            	
-            	
-            
-            return loginResultVO;
-            
-        } catch (IncorrectPasswordException e) {
-            throw e;
-        } catch (InstanceNotFoundException e) {
-            throw e;
-        } catch (InternalErrorException e) {
-            throw e;
-        } catch (Exception e) {
-            throw new InternalErrorException(e);
-        }
-            
-    }
-        
-    public UserProfileVO findUserProfile() throws InternalErrorException {
-    
-        try {
-        
-            FindUserProfileAction action = new FindUserProfileAction(loginName);
-                
-            return (UserProfileVO) PlainActionProcessor.process(getDataSource(),action);
-            
-        } catch (InstanceNotFoundException e) {
-            throw new InternalErrorException(e);
-        } catch (InternalErrorException e) {
-            throw e;
-        } catch (Exception e) {
-            throw new InternalErrorException(e);
-        }
-    
-    }
-    
-    public DireccionVO findUserDireccion() throws InternalErrorException {
-        
-        try {
-        
-            FindUserDireccionAction action = new FindUserDireccionAction(loginName);
-                
-            return (DireccionVO) PlainActionProcessor.process(getDataSource(),action);
-            
-        } catch (InstanceNotFoundException e) {
-            throw new InternalErrorException(e);
-        } catch (InternalErrorException e) {
-            throw e;
-        } catch (Exception e) {
-            throw new InternalErrorException(e);
-        }
-    
-    }
-    
-    public void updateUserProfileDetails(
-        UserProfileDetailsVO userProfileDetailsVO, Long cp, String ciudad, String direccion, Long numero)
-        throws InternalErrorException {
+	public PlainUserFacadeDelegate() {
+		loginName = null;
+	}
 
-        try {
-        
-            UpdateUserProfileDetailsAction action = 
-                new UpdateUserProfileDetailsAction(loginName, 
-                    userProfileDetailsVO, cp, ciudad, direccion, numero);
-                
-            PlainActionProcessor.process(getDataSource(), action);
-            
-        } catch (InstanceNotFoundException e) {
-            throw new InternalErrorException(e);
-        } catch (InternalErrorException e) {
-            throw e;
-        } catch (Exception e) {
-            throw new InternalErrorException(e);
-        }
-        
-    }
-      
-    public void registrarTarjeta(Long tarjeta)
-    throws InternalErrorException {
-    
-    try {
-    
-        RegistrarTarjetaAction action = new RegistrarTarjetaAction(loginName, tarjeta);
-            
-        PlainActionProcessor.process(getDataSource(), action);
-        //this.loginName = loginName;
-        
-    } catch (InternalErrorException e) {
-        throw e;
-    } catch (Exception e) {
-        throw new InternalErrorException(e);
-    }
-    
-}
-    
+	public CarroCompra getCarroCompra() throws CarroNotFoundException {
+		return carroCompra;
+	}
 
-           
-    public void changePassword(String oldClearPassword,
-        String newClearPassword) throws IncorrectPasswordException,
-        InternalErrorException {
+	//TODO el try-catch este no se como ponerlo
+	public void liberarTickets() throws ModelException, InternalErrorException {
 
-        try {
-        
-            ChangePasswordAction action = new ChangePasswordAction(loginName, 
-                oldClearPassword, newClearPassword);
-                
-            PlainActionProcessor.process(getDataSource(), action);
-            
-        } catch (IncorrectPasswordException e) {
-            throw e;
-        } catch (InstanceNotFoundException e) {
-            throw new InternalErrorException(e);
-        } catch (InternalErrorException e) {
-            throw e;
-        } catch (Exception e) {
-            throw new InternalErrorException(e);
-        }
-        
-    }
-    
-    
-   
-    public void anadirPedido(Long idPedido, Calendar fecha)
-    throws DuplicateInstanceException, InternalErrorException {
-    
-    try {
-    
-        AnadirPedidoAction action = new AnadirPedidoAction(idPedido, fecha, loginName);
-            
-        PlainActionProcessor.process(getDataSource(), action);
-        
-    } catch (DuplicateInstanceException e) {
-        throw e;
-    } catch (InternalErrorException e) {
-        throw e;
-    } catch (Exception e) {
-        throw new InternalErrorException(e);
-    }
-    }
-    
-  
-    
-    
-    private DataSource getDataSource() throws InternalErrorException {
-        return DataSourceLocator.getDataSource(
-            GlobalNames.CINES_DATA_SOURCE);
-    }
-    
-    public void liberarTicket(long idTicket) throws InternalErrorException{
-    	
-    	try{
-    		LiberarTicketAction action = new LiberarTicketAction(idTicket);
-    		
-    		//Ejecutamos la accion
-    		PlainActionProcessor.process(getDataSource(), action);
-    		
-    	}catch (ModelException e) {
+		LiberarTicketsAction action = new LiberarTicketsAction(this.loginName,
+				this.carroCompra);
+
+		try {
+
+			PlainActionProcessor.process(getDataSource(), action);
+
+		} catch (ModelException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InternalErrorException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	public void registerUser(String loginName, String clearPassword, Long cp,
+			String ciudad, String direccion, Long numero,
+			UserProfileDetailsVO userProfileDetailsVO)
+			throws DuplicateInstanceException, InternalErrorException {
+
+		try {
+
+			RegisterUserAction action = new RegisterUserAction(loginName,
+					clearPassword, cp, ciudad, direccion, numero,
+					userProfileDetailsVO);
+
+			PlainActionProcessor.process(getDataSource(), action);
+			this.carroCompra = new CarroCompra();
+			this.loginName = loginName;
+
+		} catch (DuplicateInstanceException e) {
+			throw e;
+		} catch (InternalErrorException e) {
+			throw e;
+		} catch (Exception e) {
 			throw new InternalErrorException(e);
 		}
-    } 
-    
-    public void terminar() throws InternalErrorException{
-    	try{
-    		TerminarAction action = new TerminarAction(loginName, carroCompra);
-    		
-    		PlainActionProcessor.process(getDataSource(),action);
-    	}catch (ModelException e) {
+
+	}
+
+	public LoginResultVO login(String loginName, String password,
+			boolean passwordIsEncrypted) throws InstanceNotFoundException,
+			IncorrectPasswordException, InternalErrorException {
+
+		try {
+
+			LoginAction action = new LoginAction(loginName, password,
+					passwordIsEncrypted);
+
+			LoginResultVO loginResultVO = (LoginResultVO) PlainActionProcessor
+					.process(getDataSource(), action);
+
+			this.loginName = loginName;
+			this.carroCompra = new CarroCompra();
+
+			return loginResultVO;
+
+		} catch (IncorrectPasswordException e) {
+			throw e;
+		} catch (InstanceNotFoundException e) {
+			throw e;
+		} catch (InternalErrorException e) {
+			throw e;
+		} catch (Exception e) {
 			throw new InternalErrorException(e);
 		}
-    	    	
-    }
-    public void comprarM(long id) throws InternalErrorException{
-    	try{
-    	ComprarMAction action = new ComprarMAction(id);
-    	carroCompra.addProducto(
-    	(MerchandisingVO)PlainActionProcessor.process(getDataSource(), action)
-    	);
-    	}catch (Exception e) {
+
+	}
+
+	public UserProfileVO findUserProfile() throws InternalErrorException {
+
+		try {
+
+			FindUserProfileAction action = new FindUserProfileAction(loginName);
+
+			return (UserProfileVO) PlainActionProcessor.process(
+					getDataSource(), action);
+
+		} catch (InstanceNotFoundException e) {
+			throw new InternalErrorException(e);
+		} catch (InternalErrorException e) {
+			throw e;
+		} catch (Exception e) {
 			throw new InternalErrorException(e);
 		}
-    	
-    }
-    public void comprarD(String id) throws InternalErrorException{
-    	try{
-    	ComprarDAction action = new ComprarDAction(id);
-    	carroCompra.addProducto(
-    	(dvdVO)PlainActionProcessor.process(getDataSource(), action)
-    	);
-    	}catch (Exception e) {
+
+	}
+
+	public DireccionVO findUserDireccion() throws InternalErrorException {
+
+		try {
+
+			FindUserDireccionAction action = new FindUserDireccionAction(
+					loginName);
+
+			return (DireccionVO) PlainActionProcessor.process(getDataSource(),
+					action);
+
+		} catch (InstanceNotFoundException e) {
+			throw new InternalErrorException(e);
+		} catch (InternalErrorException e) {
+			throw e;
+		} catch (Exception e) {
 			throw new InternalErrorException(e);
 		}
-    	
-    }
+
+	}
+
+	public void updateUserProfileDetails(
+			UserProfileDetailsVO userProfileDetailsVO, Long cp, String ciudad,
+			String direccion, Long numero) throws InternalErrorException {
+
+		try {
+
+			UpdateUserProfileDetailsAction action = new UpdateUserProfileDetailsAction(
+					loginName, userProfileDetailsVO, cp, ciudad, direccion,
+					numero);
+
+			PlainActionProcessor.process(getDataSource(), action);
+
+		} catch (InstanceNotFoundException e) {
+			throw new InternalErrorException(e);
+		} catch (InternalErrorException e) {
+			throw e;
+		} catch (Exception e) {
+			throw new InternalErrorException(e);
+		}
+
+	}
+
+	public void registrarTarjeta(Long tarjeta) throws InternalErrorException {
+
+		try {
+
+			RegistrarTarjetaAction action = new RegistrarTarjetaAction(
+					loginName, tarjeta);
+
+			PlainActionProcessor.process(getDataSource(), action);
+			//this.loginName = loginName;
+
+		} catch (InternalErrorException e) {
+			throw e;
+		} catch (Exception e) {
+			throw new InternalErrorException(e);
+		}
+
+	}
+
+	public void changePassword(String oldClearPassword, String newClearPassword)
+			throws IncorrectPasswordException, InternalErrorException {
+
+		try {
+
+			ChangePasswordAction action = new ChangePasswordAction(loginName,
+					oldClearPassword, newClearPassword);
+
+			PlainActionProcessor.process(getDataSource(), action);
+
+		} catch (IncorrectPasswordException e) {
+			throw e;
+		} catch (InstanceNotFoundException e) {
+			throw new InternalErrorException(e);
+		} catch (InternalErrorException e) {
+			throw e;
+		} catch (Exception e) {
+			throw new InternalErrorException(e);
+		}
+
+	}
+
+	public void anadirPedido(Long idPedido, Calendar fecha)
+			throws DuplicateInstanceException, InternalErrorException {
+
+		try {
+
+			AnadirPedidoAction action = new AnadirPedidoAction(idPedido, fecha,
+					loginName);
+
+			PlainActionProcessor.process(getDataSource(), action);
+
+		} catch (DuplicateInstanceException e) {
+			throw e;
+		} catch (InternalErrorException e) {
+			throw e;
+		} catch (Exception e) {
+			throw new InternalErrorException(e);
+		}
+	}
+
+	private DataSource getDataSource() throws InternalErrorException {
+		return DataSourceLocator.getDataSource(GlobalNames.CINES_DATA_SOURCE);
+	}
+
+	public void liberarTicket(long idTicket) throws InternalErrorException {
+
+		try {
+			LiberarTicketAction action = new LiberarTicketAction(idTicket);
+
+			//Ejecutamos la accion
+			PlainActionProcessor.process(getDataSource(), action);
+
+		} catch (ModelException e) {
+			throw new InternalErrorException(e);
+		}
+	}
+
+	public void terminar() throws InternalErrorException {
+		try {
+			TerminarAction action = new TerminarAction(loginName, carroCompra);
+
+			PlainActionProcessor.process(getDataSource(), action);
+		} catch (ModelException e) {
+			throw new InternalErrorException(e);
+		}
+
+	}
+
+	public void comprarM(long id) throws InternalErrorException {
+		try {
+			ComprarMAction action = new ComprarMAction(id);
+			carroCompra.addProducto((MerchandisingVO) PlainActionProcessor
+					.process(getDataSource(), action));
+		} catch (Exception e) {
+			throw new InternalErrorException(e);
+		}
+
+	}
+
+	public void comprarD(String id) throws InternalErrorException {
+		try {
+			ComprarDAction action = new ComprarDAction(id);
+			carroCompra.addProducto((dvdVO) PlainActionProcessor.process(
+					getDataSource(), action));
+		} catch (Exception e) {
+			throw new InternalErrorException(e);
+		}
+
+	}
 
 	public TarjetaVO findUserTarjeta() throws InternalErrorException {
-		
+
 		try {
 			FindUserTarjetaAction action = new FindUserTarjetaAction(loginName);
-			return (TarjetaVO) PlainActionProcessor.process(getDataSource(), action);
+			return (TarjetaVO) PlainActionProcessor.process(getDataSource(),
+					action);
 		} catch (ModelException e) {
 			throw new InternalErrorException(e);
 		} catch (InternalErrorException e) {
 			throw new InternalErrorException(e);
 		}
 	}
-    
-    
+
 }
